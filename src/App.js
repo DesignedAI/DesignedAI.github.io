6be1062cd40b580e047b76f516602e607c4bd664
Hyunho Son <aa000098@naver.com>
2024-03-19 15:08:47 +0900

    feat: 구글 애널리틱스 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom"
 import { ThemeProvider } from "styled-components"
+import ReactGA from "react-ga";
 
 import GlobalStyle from "./GlobalStyle"
 import theme from "./theme"
@@ -8,6 +9,8 @@ import theme from "./theme"
 import ScrollToTop from "./Components/ScrollToTop"
 import Contact from "./Pages/Contact"
 import Root from "./Pages/Root"
+
+ReactGA.pageview(window.location.pahtname);
 
 const App = () => {
     return (
--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import { BrowserRouter } from "react-router-dom"
 import { RecoilRoot } from "recoil"
 
 import App from "./App"
+import ReactGA from "react-ga";
+
+const gaTrackingId = process.env.REACT_APP_GA_TRACKING_ID;
+ReactGA.initialize(gaTrackingId, {debug: true});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <RecoilRoot>
