9dc4d0eb3022c09b021579c2a1aecb7dec0cf67e
Hyunho Son <aa000098@naver.com>
2024-03-25 16:53:27 +0900

    feat: 폰트 추가 및 구글 애널리틱스 적용

--- src/Components/RouteChangeTracker.js
+++ src/Components/RouteChangeTracker.js
@@ -1,22 +1,20 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 
 const RouteChangeTracker = () => {
-
     const location = useLocation();
     const [initialized, setInitialized] = useState(false);
 
     useEffect(() => {
-        if(!window.location.href.includes("localhost")){
-            ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID);
-        }
+        ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID);
         setInitialized(true);
     },[]);
 
     useEffect(() => {
         if(initialized){
-            ReactGA.pageview(location.pathname + location.search);
+            ReactGA.set({ page: location.pathname });
+            ReactGA.send("pageview");
         }
     },[initialized, location]);
 
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
 import { RecoilRoot } from "recoil"
-
 import App from "./App"
-import ReactGA from "react-ga";
+import ReactGA from "react-ga4";
+import "./fonts/Font.css";
 
 const gaTrackingId = process.env.REACT_APP_GA_TRACKING_ID;
-ReactGA.initialize(gaTrackingId, {debug: true});
+if (gaTrackingId) {
+    ReactGA.initialize(gaTrackingId, {debug: true});
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <RecoilRoot>
